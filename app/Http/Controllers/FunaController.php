54b4a6c947da4a69a8bb01cef36444e565d8fd26
julian.bueno <julian.bueno@twelve.net.co>
2022-05-04 15:23:49 -0500

    Public images in bitbucket

--- app/Http/Controllers/FunaController.php
+++ app/Http/Controllers/FunaController.php
@@ -8,7 +8,7 @@ const FUNAS = [
         "Nombre" => "Fuwako Yuni",
         "Altura" => 1.69,
         "Habilidad" => "Vuela por que es un alicornio",
-        "Imagen" => "https://storage.cloud.google.com/funatubers/fuwakoYuni.png?authuser=2",
+        "Imagen" => "https://storage.cloud.google.com/funatubers/fuwakoYuni.png",
         "Frase Filosofica" => "Volar o no volar, esa es la cuestion"
     ],
     [
@@ -16,7 +16,7 @@ const FUNAS = [
         "Nombre" => "Rocco Pops",
         "Altura" => 1.57,
         "Habilidad" => "Se transforma en lobo",
-        "Imagen" => "https://storage.cloud.google.com/funatubers/RoccoPops.jpg?authuser=2",
+        "Imagen" => "https://storage.cloud.google.com/funatubers/RoccoPops.jpg",
         "Frase Filosofica" => "Aullando a la luna como Michal Jackson"
     ],
     [
@@ -24,7 +24,7 @@ const FUNAS = [
         "Nombre" => "Kakashi Rushi",
         "Altura" => 1.62,
         "Habilidad" => "Esta hecha de paja de la de granja",
-        "Imagen" => "https://storage.cloud.google.com/funatubers/KakashiRushi.png?authuser=2",
+        "Imagen" => "https://storage.cloud.google.com/funatubers/KakashiRushi.png",
         "Frase Filosofica" => "Hay un hombre de paja dentro de una casa de paja, se pregunta a si mismo, esta el hombre hecho de casa o esta la casa hecha de hombre, el hombre grita por que no sabe."
     ],
     [
@@ -32,7 +32,7 @@ const FUNAS = [
         "Nombre" => "Almadeia Ciana",
         "Altura" => 1.65,
         "Habilidad" => "Desarrrolla jueguitos",
-        "Imagen" => "https://storage.cloud.google.com/funatubers/AlmadeiaCiana.jpeg?authuser=2",
+        "Imagen" => "https://storage.cloud.google.com/funatubers/AlmadeiaCiana.jpeg",
         "Frase Filosofica" => "Cuando miras al vacio, el vacio te devuelve la mirada"
     ],
     [
@@ -40,7 +40,7 @@ const FUNAS = [
         "Nombre" => "Tulip Blossom",
         "Altura" => 1.73,
         "Habilidad" => "Se monta en su ovni",
-        "Imagen" => "https://storage.cloud.google.com/funatubers/TulipBlossom.jpg?authuser=2",
+        "Imagen" => "https://storage.cloud.google.com/funatubers/TulipBlossom.jpg",
         "Frase Filosofica" => "Los tulipanes cuando florecen"
     ],
     [
@@ -48,7 +48,7 @@ const FUNAS = [
         "Nombre" => "Reina Hardgore",
         "Altura" => 1.66,
         "Habilidad" => "Juega al League of Legends",
-        "Imagen" => "https://storage.cloud.google.com/funatubers/ReinaHardgore.jpg?authuser=2",
+        "Imagen" => "https://storage.cloud.google.com/funatubers/ReinaHardgore.jpg",
         "Frase Filosofica" => "No lloren por mi, ya estoy muerto"
     ],
     [
@@ -56,7 +56,7 @@ const FUNAS = [
         "Nombre" => "Poneko",
         "Altura" => 1.80,
         "Habilidad" => "Ser un gato estilista",
-        "Imagen" => "https://storage.cloud.google.com/funatubers/POneko.jpg?authuser=2",
+        "Imagen" => "https://storage.cloud.google.com/funatubers/POneko.jpg",
         "Frase Filosofica" => "El arte es subjetivo"
     ],
     [
@@ -64,7 +64,7 @@ const FUNAS = [
         "Nombre" => "Tachbiana Mitsuki",
         "Altura" => 1.67,
         "Habilidad" => "No se, no conozca a esta funatuber xD",
-        "Imagen" => "https://storage.cloud.google.com/funatubers/TachibanaMitsuki.jpg?authuser=2",
+        "Imagen" => "https://storage.cloud.google.com/funatubers/TachibanaMitsuki.jpg",
         "Frase Filosofica" => "Se me acabaron als frases filosoficas"
     ],
     [
@@ -72,7 +72,7 @@ const FUNAS = [
         "Nombre" => "Rawberry Preserves",
         "Altura" => 1.54,
         "Habilidad" => "Devorar todo a su paso",
-        "Imagen" => "https://storage.cloud.google.com/funatubers/RawberryPreserves.jpg?authuser=2",
+        "Imagen" => "https://storage.cloud.google.com/funatubers/RawberryPreserves.jpg",
         "Frase Filosofica" => "Los insectos son amigos!"
     ],
     [
@@ -80,7 +80,7 @@ const FUNAS = [
         "Nombre" => "Etihw",
         "Altura" => 1.66,
         "Habilidad" => "Ser dios",
-        "Imagen" => "https://storage.cloud.google.com/funatubers/Etihw.jpg?authuser=2",
+        "Imagen" => "https://storage.cloud.google.com/funatubers/Etihw.jpg",
         "Frase Filosofica" => "Somos esencialmente creaturas que fundamentalmente no se pueden comprender la una a la otra."
     ]
 ];
